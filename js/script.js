0ffd2d36f5b8bda7d39f395db0f97c685efb31aa
Ethan Henderson <ethan.henderson397@gmail.com>
2020-08-02 18:40:43 -0400

    finished the mobile navbar. this was way more work than I expected.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+  $(document.getElementById('myLinks')).toggleClass('showMenu');
+  $(document.getElementById('ul')).toggleClass('showMenu');
 }
